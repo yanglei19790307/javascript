923e7e4405ee4398372019b6d2b3eacc5f7a9ce6
yanglei <god_xp@163.com>
2016-11-26 23:22:16 +0800

    123123
    123123

---
+++ 练习/lib/helloWord.js
@@ -0,0 +1,6 @@
+/**
+ * Created by yanglei on 2016/11/24.
+ */
+//alert("hello yanglei");
+//document.write("<p>My First JavaScript</p>");
+document.getElementById("p1").innerText="this is changed value";
